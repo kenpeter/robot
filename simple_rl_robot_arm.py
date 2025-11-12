ee9ec7d9eba32c14e7a9c40ad81e5d2008d9ebaf
Your Name <you@example.com>
2025-11-12 22:23:13 +1100

    WIP: update

--- simple_rl_robot_arm.py
+++ simple_rl_robot_arm.py
@@ -13,7 +13,7 @@ from isaacsim import SimulationApp
 # Initialize simulation
 simulation_app = SimulationApp(
     {
-        "headless": True,
+        "headless": False,
         "width": 1280,
         "height": 720,
         # ray trace vs path trace: ray trace -> good performance -> path trace -> more real
